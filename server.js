e23341a6f6e1e993d51086474250d50f6c886644
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-03-16 11:58:14 +0200

    ssl

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 // Routes
 
